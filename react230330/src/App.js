ba0e4601daf54b950b92e3cfd6ca4394b4fad8cd
Bonobono1222 <ririririri6@naver.com>
2023-03-30 12:01:27 +0900

    ⚡️ 오전 하단바

--- react230330/src/App.js
+++ react230330/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import BottomBar from './component/BottomBar';
+import Post from './component/Post';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Post/>
+      <BottomBar/>
     </div>
   );
 }
